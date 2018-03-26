b02fdb832b82a191f13865dcbd9e17945d5dae09
clhchtcjj <15720622991@163.com>
2018-03-26 22:13:14 +0800

    add some new problems

--- offer/offer 17 print 1-n.py
+++ offer/offer 17 print 1-n.py
@@ -4,7 +4,7 @@ __author__ = 'CLH'
 
 class Solution():
     def __init__(self):
-        self.ans = "0"
+        self.ans = ""
 
     def printOneToN(self,n):
         if n <= 0:
@@ -37,12 +37,34 @@ class Solution():
     def printAns(self):
         # 去除最高位的0
         i = 0
+        if self.ans == "0":
+            return
+        flag = False
         for i in range(0,len(self.ans)):
             if self.ans[i] != '0':
+                flag = True
                 break
-        ans = self.ans[i:]
+        if not flag:
+            ans = self.ans[i+1:]
+        else:
+            ans = self.ans[i:]
+        if ans == "":
+            return
         print(ans)
+
+    # 全排列思想
+    def fullPermutation(self,n,k):
+        if k == n:
+            self.printAns()
+        else:
+            k = k + 1
+            for i in range(0,10):
+                self.ans += str(i)
+                self.fullPermutation(n,k)
+                self.ans = self.ans[:k-1]
+
+
 
 if __name__ == "__main__":
     S = Solution()
-    S.printOneToN(0)
+    S.fullPermutation(3,0)
